43af6416fef84f160b699ff45e0f122e8b284922
Erik Nilsson <en222je@student.lnu.se>
2013-12-18 09:36:32 -0500

    Gjort add för att lägga till print.css

---
+++ pages/laboration5.js
@@ -0,0 +1,35 @@
+// Test
+//console.log("Test");
+//function toggletable() {
+    //alert ("Hello world!");
+    //}
+    
+
+funktion toggletable() {
+    var table = document.getElementById("hide");
+    var displaySet = hide.style.display;
+    var hideshow = document.getElementById("hideshow");
+
+    if (displaySet === "block") {
+        table.style.display = "none";
+    }
+    else {
+        table.style.display = "block";
+    }
+}
+
+
+//function hide_menu(table1){
+//document.getElementById(table1).style.display = "none";
+//}
+//function show_menu(table1){
+//document.getElementById(table1).style.display = "block";
+//}
+
+//function toggletable (){
+    //if (document.getElementById("table1").style.display === "none") show_menu ("table1"); 
+    //else hide_menu ("table1");
+//}
+
+
+
